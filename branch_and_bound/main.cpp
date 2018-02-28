cc575e35f12a38b0406ff0a8f77bf858bc6c5a47
mauroberto <maurorcsc@gmail.com>
2018-02-28 17:39:40 -0300

    branch and bound

--- branch_and_bound/main.cpp
+++ branch_and_bound/main.cpp
@@ -4,18 +4,22 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <time.h>
+#include "gurobi_c++.h"
 
 using namespace std;
 
-#define MAX_V_NUMBER 20000
-
-int weight[MAX_V_NUMBER][MAX_V_NUMBER];
-int group[MAX_V_NUMBER];
+vector<vector<int> > weight;
+vector<int> sets;
 vector<int> w_max;
 vector<pair<int, int> > w_vertex;
 vector<int> size;
 int K, N;
 long long int subp = 0;
+int timeout;
+double elapsed;
+double lastElapsed;
+clock_t clk;
 
 int record = 0;
 vector<int> s_max;
@@ -60,18 +64,18 @@ void readFile(char * fn) {
     N += size[i];
   }
 
+  weight.resize(N);
+  sets.resize(N);
   w_vertex.resize(N, make_pair(0,0));
 
   for(int i = 0; i < N; i++){
     w_vertex[i].second = i;
-    for(int j = 0; j < N; j++){
-      weight[i][j] = 0;
-    }
+    weight[i].resize(N, 0);
   }
 
   for(int i=0, c=0; i<K; i++){
     for(int j=0; j<size[i]; j++){
-      group[c + j] = i;
+      sets[c + j] = i;
     }
     w_max[i] = c+i;
     c += size[i];
@@ -87,8 +91,8 @@ void readFile(char * fn) {
   }
 
   for(int i=0; i<N; i++){
-    if(w_vertex[w_max[group[i]]].first < w_vertex[i].first){
-      w_max[group[i]] = i;
+    if(w_vertex[w_max[sets[i]]].first < w_vertex[i].first){
+      w_max[sets[i]] = i;
     }
   }
 }
@@ -141,10 +145,10 @@ int upper_bound(vector<int> s, vector<int> c){
   int size_c = c.size();
   int size_s = s.size();
 
-  int group_UB[K];
+  int sets_UB[K];
 
   for(int i = 0; i < K; i++){
-    group_UB[i] = 0;
+    sets_UB[i] = 0;
   }
 
   for(int i = 0; i < size_c; i++){
@@ -157,70 +161,176 @@ int upper_bound(vector<int> s, vector<int> c){
       w += weight[c[i]][s[j]];
     }
 
-    if(group_UB[group[c[i]]] < w){
-      group_UB[group[c[i]]] = w;
+    if(sets_UB[sets[c[i]]] < w){
+      sets_UB[sets[c[i]]] = w;
     }
   }
 
   int value = 0;
 
   for(int i = 0; i < K; i++){
-    value += group_UB[i];
+    value += sets_UB[i];
   }
 
   return value;
 }
 
-void branch_and_bound(vector<int> s, vector<int> c){
+double upper_bound_linear_relaxation(vector<int> s){
+
+	GRBEnv env = GRBEnv();
+
+	GRBModel model = GRBModel(env);
+
+	vector<GRBVar> X;
+
+    for(int i = 0; i < N; i++){
+ 		   X.push_back(model.addVar(0.0, 1.0, 0.0, GRB_CONTINUOUS, "x"));
+    }
+
+    vector<vector <GRBVar> > Y (N, vector<GRBVar>(N));
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++) {
+            Y[i][j] = (model.addVar(0.0, 1.0, 0.0, GRB_CONTINUOUS, "y"));
+        }
+    }
+
+    GRBLinExpr expr = 0.0;
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            expr += weight[i][j] * Y [i][j];
+        }
+    }
+
+
+    //01
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            if(weight[i][j] == 0) continue;
+            model.addConstr(Y[i][j] <= X[i], "c1");
+            model.addConstr(Y[i][j] <= X[j], "c1-2");
+        }
+    }
+
+
+    //02
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            if(weight[i][j] == 0) continue;
+            model.addConstr(X[i] + X[j] <= Y[i][j] + 1,"c2");
+        }
+    }
+
+    //03
+    int k = 0;
+    for(int i = 0; i < K; i++){
+        GRBLinExpr expr1 = 0.0;
+        for(int j = 0; j < size[i]; j++, k++){
+            expr1 += X[k];
+        }
+
+        model.addConstr(expr1 <= 1, "c3-s-");
+    }
+
+    //04
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            if(weight[i][j] != 0) continue;
+            model.addConstr(X[i] + X[j] <= 1, "c3");
+        }
+    }
+
+
+    for(int i = 0; i < s.size(); i++){
+      int index = s[i];
+    	model.addConstr(X[index] == 1, "force");
+    }
+
+    //set variables
+
+    //cout << "running linear relaxation" << endl;
+
+    model.set(GRB_IntParam_OutputFlag, 0);
+    model.setObjective(expr, GRB_MAXIMIZE);
+    model.optimize();
+
+    //cout << "best bound from linear relaxation " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+
+    return model.get(GRB_DoubleAttr_ObjVal);
+}
+
+void branch_and_bound(vector<int> s, vector<int> c, int level){
   subp++;
 
   int size_s = s.size();
   int size_c = c.size();
 
+
   int value = 0;
+
   for(int i = 0; i < size_s; i++){
     for(int j = i+1; j < size_s; j++){
       value += weight[s[i]][s[j]];
     }
   }
 
-  value = value;
+  elapsed = ((double) (clock() - clk)) / CLOCKS_PER_SEC;
+
+  if(elapsed > timeout){
+    cout << "Record: " << record << endl;
+    cout << "Subp: " << subp << endl;
+    cout << fixed << "Time " << elapsed << endl;
+
+
+    int size_s = s_max.size();
+    for(int i=0; i<size_s; i++){
+      cout << s_max[i] << " ";
+    }
+
+    cout << endl;
+
+    exit(1);
+  }
+
+  /*if(elapsed - lastElapsed > 5){
+    cout << "solution value: " << record << " | Subp " << subp << fixed << " | Time " << elapsed << "s" << endl;
+    lastElapsed = elapsed;
+  }*/
 
   if(size_s == K){
     if(value > record){
-      cout << "improving solution value: " << value << endl;
+      cout << "solution value: " << value << " | Subp " << subp << fixed << " | Time " << elapsed << "s" << endl;
+      lastElapsed = elapsed;
       record = value;
       s_max.clear();
       for(int i = 0; i < size_s; i++){
         s_max.push_back(s[i]);
       }
     }
-  }
 
-  int ub = upper_bound(s, c);
-
-  if(ub + value <= record){
     return;
   }
 
-  vector<int> new_s(size_s+1);
+  int ub = upper_bound(s, c) + value;
+
+  if(ub <= record){
+    return;
+  }
+
+  vector<int> new_s(level+1);
   vector<int> new_c;
 
-  for(int i = 0; i < size_s; i++){
+  for(int j = size[level]; j < size_c; j++){
+    new_c.push_back(c[j]);
+  }
+
+  for(int i = 0; i < level; i++){
     new_s[i] = s[i];
   }
 
-  for(int i = 0; i < size_c; i++){
-    new_s[size_s] = c[i];
-    new_c.clear();
+  for(int i = 0; i < size[level]; i++){
+    new_s[level] = c[i];
 
-    for(int j = 0; j < size_c; j++){
-      if(group[c[i]] != group[c[j]]){
-        new_c.push_back(c[j]);
-      }
-    }
-
-    branch_and_bound(new_s, new_c);
+    branch_and_bound(new_s, new_c, level+1);
   }
 
 }
@@ -234,21 +344,29 @@ int main(int argc, char *argv[]){
 
   readFile(argv[1]);
 
+  clk = clock();
+  cout.precision(2);
+
+  timeout = 3600;
+
   vector<int> s;
   vector<int> c;
-
-  for(int i = 0; i < N; i++){
-    c.push_back(w_vertex[i].second);
-  }
-
 
   if(argc > 2){
     readFileSolution(argv[2]);
   }else{
     lower_bound();
   }
+
   //SORTING VERTICES
-  sort(w_vertex.begin(), w_vertex.end(), greater<pair<int, int> >());
+  for(int i=0, count=0; i<K; i++){
+    sort(w_vertex.begin()+count, w_vertex.begin()+count+size[i], greater<pair<int, int> >());
+    count += size[i];
+  }
+
+  for(int i = 0; i < N; i++){
+    c.push_back(w_vertex[i].second);
+  }
 
   cout << "LB " << record << endl;
 
@@ -256,10 +374,14 @@ int main(int argc, char *argv[]){
 
   cout << "UB " << ub << endl;
 
-  branch_and_bound(s, c);
+  lastElapsed = 0;
 
-  cout << record << endl;
-  cout << subp << endl;
+  branch_and_bound(s, c, 0);
+
+  elapsed = ((double) (clock() - clk)) / CLOCKS_PER_SEC;
+  cout << "Record: " << record << endl;
+  cout << "Subp: " << subp << endl;
+  cout << fixed << "Time " << elapsed << endl;
 
 
   int size_s = s_max.size();
