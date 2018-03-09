698a5337b707208c52b7f76066f487e6b9431d35
italosestilon <italosestilon@gmail.com>
2018-03-09 00:38:20 -0300

    gurobi path updated

--- integer_programming/run-me.sh
+++ integer_programming/run-me.sh
@@ -1,4 +1,4 @@
-export GUROBI_HOME="/opt/gurobi/752/linux64"
+export GUROBI_HOME="/opt/gurobi752/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE="$HOME/gurobi.lic"
