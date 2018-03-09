ff8fa267c9efcacb6824f0020012738b3554c90a
italosestilon <italosestilon@gmail.com>
2018-03-09 00:59:39 -0300

    remove command make from the run script

--- integer_programming/run-me.sh
+++ integer_programming/run-me.sh
@@ -3,9 +3,6 @@ export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE="$HOME/gurobi.lic"
 
-make clean
-make
-
 rm -rf result
 mkdir result
 
