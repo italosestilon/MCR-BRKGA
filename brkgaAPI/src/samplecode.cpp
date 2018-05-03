efe48db6e4e9b44cd788be7e5bb99e217819f1e9
mauroberto <maurorcsc@gmail.com>
2018-05-03 20:19:19 -0300

    testes

--- brkgaAPI/src/samplecode.cpp
+++ brkgaAPI/src/samplecode.cpp
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
 		unsigned generation = 0;		// current generation
 		const unsigned X_INTVL = 100;	// exchange best individuals at every 100 generations
 		const unsigned X_NUMBER = 2;	// excichange top 2 best
-		const unsigned MAX_GENS = 500;	// run for 1000 gens
+		const unsigned MAX_GENS = 1000;	// run for 1000 gens
 
 		clk = clock();
 		do {
